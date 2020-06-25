920a17f77ffd0db2f9c61cdab5e30b397436c815
hiromichi shinohara <hiromichishinohara@hiromichinoMacBook-Pro.local>
2020-06-26 07:12:03 +0900

    1

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 'use strict'
 
 {
-  document.querySelector('button').addEventListener('click', () => {document.getElementById('target').textContent = 'Changed!';
+  document.querySelector('button').addEventListener('click', () => {
+    const targetNode = document.getElementById('target');
+    
+    // targetNode.className = "my-color my-border";
+    // targetNode.classList.add("my-color");
+  //   if(targetNode.classList.contains("my-color")=== true) {
+  //     targetNode.classList.remove("my-color");
+  //   } else{
+  //     targetNode.classList.add("my-color");
+  //   }
+  // });
+  targetNode.classList.toggle("my-color");
   });
 }
