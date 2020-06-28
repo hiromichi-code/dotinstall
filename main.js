1122e502de202916a942851a5fa1347f45d22728
hiromichi shinohara <hiromichishinohara@hiromichinoMacBook-Pro.local>
2020-06-29 06:10:20 +0900

    1

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-'use strict'
+'use strict';
 
 {
-  document.querySelector('button').addEventListener('click', () => {
-    const item0 = document.querySelectorAll('li')[0];
-    const copy = item0.cloneNode(true);
+  const btn = document.getElementById('btn');
 
-    const ul = document.querySelector('ul');
-    const item2 = document.querySelectorAll('li')[2];
-    ul.insertBefore(copy, item2);
+  btn.addEventListener('click', () => {
+    btn.textContent = 'hit!';
   });
 }
