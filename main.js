9548a25234fd9ac407c8b1ac72eb0a8ebe286ae9
hiromichi shinohara <hiromichishinohara@hiromichinoMacBook-Pro.local>
2020-07-04 07:12:44 +0900

    1

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 'use strict';
 
 {
-  const btn = document.getElementById('btn');
-
-  btn.addEventListener('click', () => {
-    // const results = ['大吉','中吉','小吉','凶']
-    // const results = ['大吉','大吉','大吉','凶']
-    // btn.textContent = results[Math.floor(Math.random() * results.length)];
-    const n = Math.random();
-    if(n < 0.02){
-      btn.textContent = '凶';
-    }else if(n < 0.2){
-      btn.textContent = '中吉';
-    }else{
-      btn.textContent = '大吉';
+  function createColumn(col){
+    const source = [];
+    for(let i = 0; i < 15; i++){
+      source[i] = i + 1 + 15 * col;
     }
-  });
+    
+    const column = [];
+    for(let i = 0; i < 5; i++){
+      column[i] = source.splice(Math.floor(Math.random()*source.length), 1)[0];
+    }
+    
+    return column;
+  }
+
+  function createColumns(){
+    const columns = [];
+    for(let i = 0; i < 5; i++){
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
+  }
+
+  function renderBingo(columns){
+    for(let row = 0; row < 5; row++){
+      const tr = document.createElement('tr');
+      for(let col = 0; col < 5; col++){
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
+
 }
